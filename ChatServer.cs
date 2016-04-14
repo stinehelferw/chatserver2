c6b37eefe29c7b29090ec694fdd34921b63e92d9
stinehelferw <stinehelferw@gmail.com>
2016-04-13 20:07:55 -0400

    ChatServer2
    initial release

---
+++ ChatServer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace ChatServer
+{
+    public class ChatServer
+    {
+        private static ChatServer ChatServerInstance;
+        static readonly object padlock = new object();
+
+        private ChatServer()
+        {
+
+        }
+        public static ChatServer GetInstance()
+        {
+            lock (padlock)
+            {
+                if (ChatServerInstance == null)
+                {
+                    ChatServerInstance = new ChatServer();
+                }
+                return ChatServerInstance;
+            }
+        }
+
+        List<Thread> list = new List<Thread>();
+
+        private IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+        private Int32 port = 2223;
+
+        public void serverListen()
+        {
+            TcpListener tcpListener = new TcpListener(localAddr, port);
+            tcpListener.Start();
+
+            TcpClient tcpClient = tcpListener.AcceptTcpClient();
+
+        }
+    }
+}
---
+++ ChatWindow.designer.cs
@@ -0,0 +1,83 @@
+﻿namespace ChatServer
+{
+    partial class ChatWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outputTxt = new System.Windows.Forms.TextBox();
+            this.inputTxt = new System.Windows.Forms.TextBox();
+            this.submitBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // outputTxt
+            // 
+            this.outputTxt.Location = new System.Drawing.Point(12, 35);
+            this.outputTxt.Multiline = true;
+            this.outputTxt.Name = "outputTxt";
+            this.outputTxt.ReadOnly = true;
+            this.outputTxt.Size = new System.Drawing.Size(410, 196);
+            this.outputTxt.TabIndex = 0;
+            // 
+            // inputTxt
+            // 
+            this.inputTxt.Location = new System.Drawing.Point(12, 237);
+            this.inputTxt.Multiline = true;
+            this.inputTxt.Name = "inputTxt";
+            this.inputTxt.Size = new System.Drawing.Size(329, 63);
+            this.inputTxt.TabIndex = 1;
+            // 
+            // submitBtn
+            // 
+            this.submitBtn.Location = new System.Drawing.Point(347, 237);
+            this.submitBtn.Name = "submitBtn";
+            this.submitBtn.Size = new System.Drawing.Size(75, 23);
+            this.submitBtn.TabIndex = 2;
+            this.submitBtn.Text = "Submit";
+            this.submitBtn.UseVisualStyleBackColor = true;
+            // 
+            // ChatWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 312);
+            this.Controls.Add(this.submitBtn);
+            this.Controls.Add(this.inputTxt);
+            this.Controls.Add(this.outputTxt);
+            this.Name = "ChatWindow";
+            this.Text = "ChatWindow";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox outputTxt;
+        private System.Windows.Forms.TextBox inputTxt;
+        private System.Windows.Forms.Button submitBtn;
+    }
+}
